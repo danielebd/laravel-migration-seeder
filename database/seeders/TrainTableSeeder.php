9d8c69496baafeecd095fb732c5c4b3aea0299a2
danielebd <danielebalebalestro@gmail.com>
2023-05-08 17:17:51 +0200

    ciclo for

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -16,17 +16,19 @@ class TrainTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $newTrain = new Trains();
-        $newTrain->company = $faker->randomElement(['Trenitalia', 'Italo', 'Trenord']);
-        $newTrain->departure_station = $faker->city();
-        $newTrain->arrival_station = $faker->city();
-        $newTrain->departure_date = $faker->date('Y:m:d');
-        $newTrain->departure_time = $faker->time('H:i:s');
-        $newTrain->arrival_time = $faker->time('H:i:s');
-        $newTrain->train_code = $faker->bothify('######');
-        $newTrain->carriage_number = $faker->numberBetween(0, 15);
-        $newTrain->on_time = $faker->numberBetween(0, 1);
-        $newTrain->cancelled = $faker->numberBetween(0, 1);
-        $newTrain->save();
+        for ($i = 0; $i < 5; $i++) {
+            $newTrain = new Trains();
+            $newTrain->company = $faker->randomElement(['Trenitalia', 'Italo', 'Trenord']);
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_date = $faker->date('2023:05:d');
+            $newTrain->departure_time = $faker->time('H:i:s');
+            $newTrain->arrival_time = $faker->time('H:i:s');
+            $newTrain->train_code = $faker->bothify('######');
+            $newTrain->carriage_number = $faker->numberBetween(0, 15);
+            $newTrain->on_time = $faker->numberBetween(0, 1);
+            $newTrain->cancelled = $faker->numberBetween(0, 1);
+            $newTrain->save();
+        }
     }
 }
